94f33148f468b6a04878c1a819037a9775575bdd
see-coder <imrakesh.cse@gmail.com>
2022-09-06 11:28:35 +0530

    depth-first-values implemented using iteration

---
+++ 001-depth-first-values/iterative_sol.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def depth_first_values(root):
+    if not root:
+        return []
+    
+    stack = [ root ]
+    values = []
+
+    while stack:
+        node = stack.pop()
+        values.append(node.val)
+        if node.right:
+            stack.append(node.right)
+        if node.left:
+            stack.append(node.left)
+
+    return values
+
+
+# Testing
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')        
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+print(depth_first_values(a)) # -> ['a', 'b', 'd', 'e', 'c', 'f']
+
+# n = number of nodes
+# Time: O(n)
+# Space: O(n)
